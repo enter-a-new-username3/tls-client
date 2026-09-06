69fb86dc8571014b8d10ef232b14471f703e34c0
Yasiiin Bey <whoami@protonmail.com>
2026-09-06 16:40:23 +0300

    fixed somethings

--- examples/example1 - preset.py
+++ examples/example1 - preset.py
@@ -11,30 +11,3 @@ session = tls_client.Session(
     random_tls_extension_order=False
 )
 
-res = session.get(
-    "https://www.rivalo.bet.br/api/offer/v3/categories?sport=Football",
-    headers = {
-        "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:153.0) Gecko/20100101 Firefox/153.0",
-        "accept": "application/json",
-        "accept-language": "en-US,en;q=0.9",
-        "accept-encoding": "gzip, deflate, br, zstd",
-        "referer": "https://www.rivalo.bet.br/pt/sportsbook",
-        "credentials": "include",
-        "x-correlation-id": str(uuid.uuid4()),
-        "x-request-id": str(uuid.uuid4()),
-        "x-betr-operator": "matchserv",
-        "x-betr-brand": "rivalo.bet.br",
-        "x-locale": "en",
-        "x-session-id": str(uuid.uuid4()),
-        "sec-gpc": "1",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "priority": "u=4",
-        "pragma": "no-cache",
-        "cache-control": "no-cache",
-        "te": "trailers",
-    },
-    proxy="a:a@localhost:8083",
-    verify=False
-)
